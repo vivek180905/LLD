8db7e75a609fd2d031ced74d566b37a30dfffb54
Vivek Raj <viraj9999singh@gmail.com>
2026-02-24 18:33:52 +0530

    Implement LSP with violation of OCP in account handling
    The code attempts to follow the Liskov Substitution Principle (LSP) but violates the Open/Closed Principle (OCP) by checking account types explicitly for withdrawal permissions.

---
+++ Lecture5/LSP_followed_wrongly.cpp
@@ -0,0 +1,125 @@
+// in this we will try to follow the LSP 
+// to followe we are first checking the type of account if its not the FIXED type then only we are allowing the withdrawal
+// but the problem is if we have to add any other bank type in which withdrawal is not allowed we will need to change the accounts code again
+// the change in the codes voilated teh OCP 
+// so overall to follow one principal we are voilating the other
+
+#include <iostream>
+#include <vector>
+#include <typeinfo>
+#include <stdexcept>
+
+using namespace std;
+
+class Account {
+public:
+    virtual void deposit(double amount) = 0;
+    virtual void withdraw(double amount) = 0;
+};
+
+class SavingAccount : public Account {
+private:
+    double balance;
+
+public:
+    SavingAccount() { 
+        balance = 0; 
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Savings Account. New Balance: " << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << " from Savings Account. New Balance: " << balance << endl;
+        } else {
+            cout << "Insufficient funds in Savings Account!\n";
+        }
+    }
+};
+
+class CurrentAccount : public Account {
+private:
+    double balance;
+
+public:
+    CurrentAccount() { 
+        balance = 0; 
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Current Account. New Balance: " << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            cout << "Withdrawn: " << amount << " from Current Account. New Balance: " << balance << endl;
+        } else {
+            cout << "Insufficient funds in Current Account!\n";
+        }
+    }
+};
+
+class FixedTermAccount : public Account {
+private:
+    double balance;
+
+public:
+    FixedTermAccount() { 
+        balance = 0;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: " << amount << " in Fixed Term Account. New Balance: " << balance << endl;
+    }
+
+    void withdraw(double amount) {
+        throw logic_error("Withdrawal not allowed in Fixed Term Account!");
+    }
+};
+
+//Client class
+class BankClient {
+private:
+    vector<Account*> accounts;
+
+public:
+    BankClient(vector<Account*> accounts) { 
+        this->accounts = accounts; 
+    }
+
+    void processTransactions() {
+        for (Account* acc : accounts) {
+            acc->deposit(1000);
+
+            //Checking account type explicitly
+            if (typeid(*acc) == typeid(FixedTermAccount)) {
+                cout << "Skipping withdrawal for Fixed Term Account.\n";
+            } else {
+                try {
+                    acc->withdraw(500);
+                } catch (const logic_error& e) {
+                    cout << "Exception: " << e.what() << endl;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    vector<Account*> accounts;
+    accounts.push_back(new SavingAccount());
+    accounts.push_back(new CurrentAccount());
+    accounts.push_back(new FixedTermAccount());
+
+    BankClient* client = new BankClient(accounts);
+    client->processTransactions();
+
+    return 0;
+}
